88fc9fc9c0756a7eba9f00767b8bb351a565e98d
MuriloRibas <muriloribasss@hotmail.com>
2020-11-20 16:24:43 -0300

    Change .sql data types for dates.

--- banco-dados/banco-dados.sql
+++ banco-dados/banco-dados.sql
@@ -6,16 +6,16 @@ CREATE TABLE medico(
     email 			VARCHAR(35) UNIQUE NOT NULL,
     nome			VARCHAR(50) NOT NULL,
     senha			CHAR(60) NOT NULL,
-    data_criacao	DATETIME DEFAULT CURRENT_TIMESTAMP,
-    data_alteracao	DATETIME ON UPDATE CURRENT_TIMESTAMP
+    data_criacao	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_alteracao	TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE horarios(
 	id 					INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
     id_medico			INT NOT NULL,
-    data_horario		DATETIME NOT NULL,
-    horario_agendado	TIME NOT NULL,
-    data_criacao		DATETIME DEFAULT CURRENT_TIMESTAMP,
-    data_alteracao		DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    data_horario		TIMESTAMP NOT NULL,
+    horario_agendado	INT NOT NULL,
+    data_criacao		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_alteracao		TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (id_medico) REFERENCES medico(id)
 );
